5076edce549ab7d4a640e6a25fb553fa9748ae11
alexf <alexf@5047d55a-7322-deae-15ed-1a0dbd3fe761>
2010-10-20 14:31:04 +0000

    added stereo payoffs code

---
+++ source/stereo_payoffs.cpp
@@ -0,0 +1,453 @@
+#include "stereo_payoffs.h"
+
+#include <LU.h>
+
+#include "common_types.h"
+#include "manhattan_dp.h"
+#include "camera.h"
+#include "image_utils.h"
+#include "canvas.h"
+#include "timer.h"
+
+#include "vector_utils.tpp"
+#include "integral_col_image.tpp"
+
+namespace indoor_context {
+
+	inline void toImageRef(const Vec2& v, ImageRef& p) {
+		p.x = v[0];
+		p.y = v[1];
+	}
+
+	inline void toImageRef(const Vec3& v, ImageRef& p) {
+		p.x = v[0]/v[2];
+		p.y = v[1]/v[2];
+	}
+
+	void HomographyTransform(const ImageF& input,
+													 MatF& output,
+													 const Mat3& h) {  // h transforms from input to output coords
+		ImageRef p;
+		Mat3 hinv = LU<3>(h).get_inverse();
+		for (int y = 0; y < output.Rows(); y++) {
+			float* outrow = output[y];
+			for (int x = 0; x < output.Cols(); x++) {
+				toImageRef(project(hinv * makeVector(x,y,1.0)), p);
+				if (p.x >= 0 && p.x < input.GetWidth() &&
+						p.y >= 0 && p.y < input.GetHeight()) {
+					outrow[x] = input[p].y;
+				}
+			}
+		}
+	}	
+
+
+
+
+	ostream& operator<<(ostream& o, const NCCStatistics& stats) {
+		o << format("{sum_a=%f, sum_b=%f, sum_asqr=%f, sum_bsqr=%f, sum_ab=%f, sum_wts=%f}")
+			% stats.sum_a % stats.sum_b % stats.sum_asqr % stats.sum_bsqr % stats.sum_ab % stats.sum_wts;
+		return o;
+	}
+
+	double NCCStatistics::CalculateNCC() {
+		return CalculateNCC(sum_a, sum_b, sum_asqr, sum_bsqr, sum_ab, sum_wts);
+	}
+
+	// static
+	double NCCStatistics::CalculateNCC(double sum_a,
+																		 double sum_b,
+																		 double sum_asqr,
+																		 double sum_bsqr,
+																		 double sum_ab,
+																		 double sum_wts) {
+		if (sum_wts == 0) {
+			return 0.0;
+		} else {
+			double denom_a = sum_asqr - sum_a*sum_a/sum_wts;
+			double denom_b = sum_bsqr - sum_b*sum_b/sum_wts;
+			CHECK_GE(denom_a, -1e-8) << format("{sum_a=%f, sum_asqr=%f, sum_wts=%f}") % sum_a, sum_asqr, sum_wts;
+			CHECK_GE(denom_b, -1e-8) << format("{sum_b=%f, sum_asqr=%f, sum_wts=%f}") % sum_b, sum_bsqr, sum_wts;
+			if (denom_a < 1e-6 || denom_b < 1e-8) {
+				return 0.0;  // not sure if this is really justified...
+			} else {
+				return (sum_ab - sum_a*sum_b/sum_wts) / sqrt(denom_a*denom_b);
+			}
+		}
+	}
+
+
+
+
+
+
+
+	void FastNCC::Compute(const MatF& a, const MatF& b) {
+		ComputeInternal(a, b, NULL);
+	}
+
+	void FastNCC::Compute(const MatF& a, const MatF& b, const MatI& mask) {
+		ComputeInternal(a, b, &mask);
+	}
+
+	void FastNCC::Compute(const MatF& a,
+												const MatF& b,
+												const MatF& asqr,
+												const MatF& bsqr,
+												const MatF& ab,
+												const MatI& nsamples) {
+		intg_a.Compute(a);
+		intg_b.Compute(b);
+		intg_asqr.Compute(asqr);
+		intg_bsqr.Compute(bsqr);
+		intg_ab.Compute(ab);
+		intg_nsamples.Compute(nsamples);
+	}
+
+	void FastNCC::ComputeInternal(const MatF& a, const MatF& b, const MatI* mask) {
+		CHECK_EQ(a.Rows(), b.Rows());
+		CHECK_EQ(a.Cols(), b.Cols());
+		asqr.Resize(a.Rows(), a.Cols(), 0.0);
+		bsqr.Resize(a.Rows(), a.Cols(), 0.0);
+		ab.Resize(a.Rows(), a.Cols(), 0.0);
+		nsamples.Resize(a.Rows(), a.Cols(), 0);
+		for (int y = 0; y < a.Rows(); y++) {
+			const float* a_row = a[y];
+			const float* b_row = b[y];
+			float* asqr_row = asqr[y];
+			float* bsqr_row = bsqr[y];
+			float* ab_row = ab[y];
+			int* sample_row = nsamples[y];
+			const int* mask_row = mask == NULL ? NULL : (*mask)[y];
+			for (int x = 0; x < a.Cols(); x++) {
+				if (mask == NULL || mask_row[x]) {
+					asqr_row[x] = a_row[x] * a_row[x];
+					bsqr_row[x] = b_row[x] * b_row[x];
+					ab_row[x] = a_row[x] * b_row[x];
+					sample_row[x] = 1;
+				} else {
+					CHECK_EQ(a_row[x], 0) << "inputs must be zero whereever mask is zero";
+					CHECK_EQ(b_row[x], 0) << "inputs must be zero whereever mask is zero";
+				}
+			}
+		}
+		Compute(a, b, asqr, bsqr, ab, nsamples);
+	}
+
+	void FastNCC::AddStats(int col, int row0, int row1, NCCStatistics& stats) {
+		CHECK_GT(intg_a.ny(), 0) << "AddStats() must not be called before Compute()";
+		stats.sum_a += intg_a.Sum(col, row0, row1);
+		stats.sum_b += intg_b.Sum(col, row0, row1);
+		stats.sum_asqr += intg_asqr.Sum(col, row0, row1);
+		stats.sum_bsqr += intg_bsqr.Sum(col, row0, row1);
+		stats.sum_ab += intg_ab.Sum(col, row0, row1);
+		stats.sum_wts += intg_nsamples.Sum(col, row0, row1);	// This is _not_ necessarily equal to row1-row0 !
+	}
+
+	double FastNCC::CalculateNCC(int col, int row0, int row1) {
+		NCCStatistics stats;
+		AddStats(col, row0, row1, stats);
+		return stats.CalculateNCC();
+	}
+
+
+
+
+	void HomographyNCC::AddStats(int col, int row0, int row1, NCCStatistics& stats) {
+		ncc.AddStats(col, row0, row1, stats);
+	}
+
+	double HomographyNCC::CalculateNCC(int col, int row0, int row1) {
+		return ncc.CalculateNCC(col, row0, row1);
+	}
+
+	void HomographyNCC::Compute(const ImageF& left,
+															const ImageF& right,
+															const Mat3& left_xfer,
+															const Mat3& right_xfer,
+															const Vec2I& bounds) {
+		int nx = left.GetWidth();
+		int ny = left.GetHeight();
+		left_xfered.Resize(ny, nx, 0);
+		right_xfered.Resize(ny, nx, 0);
+		mask.Resize(ny, nx, 0);
+
+		ImageRef l, r;
+		Vec3I p = makeVector(0,0,1);
+		for (p[1] = 0; p[1] < ny; p[1]++) {
+			float* left_row = left_xfered[ p[1] ];
+			float* right_row = right_xfered[ p[1] ];
+			int* mask_row = mask[ p[1] ];
+			for (p[0] = 0; p[0] < nx; p[0]++) {
+				if (left_row[ p[0] ] >= 0) { // left_row[x] < 0 indicates missing data
+					toImageRef(left_xfer*p, l);
+					if (l.x >= 0 && l.x < nx &&	l.y >= 0 && l.y < ny) {
+						toImageRef(right_xfer*p, r);
+						if (r.x >= 0 && r.x < nx &&	r.y >= 0 && r.y < ny) {
+							// important to only set these when mask is also being set to 1
+							left_row[ p[0] ] = left[l.y][l.x].y;
+							right_row[ p[0] ] = right[r.y][r.x].y;
+							mask_row[ p[0] ] = 1;
+						}
+					}
+				}
+			}
+		}
+
+		ncc.Compute(left_xfered, right_xfered, mask);
+	}
+
+	void HomographyNCC::OutputTransferredLeft(const string& file) {
+		WriteMatrixImageRescaled(file, left_xfered);
+	}
+
+	void HomographyNCC::OutputTransferredRight(const string& file) {
+		WriteMatrixImageRescaled(file, right_xfered);
+	}
+
+
+
+
+
+	void StereoPayoffs::Compute(const PosedImage& l_image,
+															const PosedImage& r_image,
+															const DPGeometry& geom,  // for left image
+															double zfloor,
+															double zceil) {
+		l_input = &l_image;
+		r_input = &r_image;
+		geometry = &geom;
+		ImageRef l_sample, r_sample;
+		Vec2I l_bounds = asToon(l_image.pc().image_size());
+
+		// Compute manhattan homologies
+		Mat3 l_fToC = GetManhattanHomology(l_image.pc(), zfloor, zceil);
+		Mat3 l_cToF = LU<3>(l_fToC).get_inverse();
+
+		// Compute vertical rectifiers
+		l_vrect = GetVerticalRectifier(l_image.pc());
+		l_vrect_inv = LU<3>(l_vrect).get_inverse();
+
+		// Compute manhattan homologies in rectified domain
+		Mat3 l_vrect_fToC = l_vrect * l_fToC * l_vrect_inv;
+		Mat3 l_vrect_cToF = l_vrect * l_cToF * l_vrect_inv;
+
+		// Compute vertical rectifiers
+		Mat3 r_vrect = GetVerticalRectifier(r_image.pc());
+		Mat3 r_vrect_inv = LU<3>(r_vrect).get_inverse();
+
+		// Compute horizon
+		Vec3 l_horizon = l_image.pc().GetImageHorizon();
+		Vec3 l_vrect_horizon = l_horizon * l_vrect_inv;
+		double l_vrect_horizon_y = -l_vrect_horizon[2] / l_vrect_horizon[1];
+
+		// Get the camera matrix for the left canera
+		const PosedCamera& l_pc = l_image.pc();
+		Mat3 l_intr = reinterpret_cast<const LinearCamera&>(l_pc.camera()).intrinsics();
+		toon::Matrix<3,4> l_cam = l_image.pc().Linearize();
+
+		// Construct the floor and ceiling planes
+		Vec4 floor_plane = makeVector(0, 0, -1, zfloor);
+		Vec4 ceil_plane = makeVector(0, 0, -1, zceil);
+
+		// Compute transfer homographies
+		Mat3 ltr_floor = GetHomographyVia(l_image.pc(), r_image.pc(), floor_plane);
+		Mat3 ltr_ceil = GetHomographyVia(l_image.pc(), r_image.pc(), ceil_plane);
+
+		// Compute integral images for floor and ceiling transfer homographies
+		floor_ncc.Compute(l_image.mono, r_image.mono, l_vrect_inv, ltr_floor*l_vrect_inv, l_bounds);
+		ceil_ncc.Compute(l_image.mono, r_image.mono, l_vrect_inv, ltr_ceil*l_vrect_inv, l_bounds);
+
+		// Rectify and transpose the intensity images
+		Mat3 tr = Zeros;
+		tr[0][1] = tr[1][0] = tr[2][2] = 1.0;
+		l_vrect_im_tr.Resize(l_image.nx(), l_image.ny(), -1);  // dimensions here are for *transposed* image
+		r_vrect_im_tr.Resize(l_image.nx(), l_image.ny(), -1);  // dimensions here are for *transposed* image
+		HomographyTransform(l_image.mono, l_vrect_im_tr, tr*l_vrect);
+		HomographyTransform(r_image.mono, r_vrect_im_tr, tr*r_vrect);
+
+		// Check that grid_floorToCeil is a pure scale+translation of y coordinates
+		CHECK_EQ_TOL(geom.grid_floorToCeil[0][0], 1.0, 1e-8);  // no scaling in x
+		CHECK_LE(abs(geom.grid_floorToCeil[0][1]), 1e-8);
+		CHECK_LE(abs(geom.grid_floorToCeil[0][2]), 1e-8);  // no translation in x
+		CHECK_LE(abs(geom.grid_floorToCeil[1][0]), 1e-8);
+		CHECK_LE(abs(geom.grid_floorToCeil[2][0]), 1e-8);
+		CHECK_LE(abs(geom.grid_floorToCeil[2][1]), 1e-8);
+		CHECK_EQ_TOL(geom.grid_floorToCeil[2][2], 1.0, 1e-8);  // normalised
+		double grid_fToC_sy = geom.grid_floorToCeil[1][1];
+		double grid_fToC_ty = geom.grid_floorToCeil[1][2];
+
+		toon::Matrix<3,2> curry_x = Zeros;
+		curry_x[1][0] = curry_x[2][1] = 1.0;
+
+		// Specify some points to visualize
+		/*MatI viz_mask(geom.grid_size[1], geom.grid_size[0], 0);
+		//int viz_pts[] = {44,282,  99,214,  210,195,  260,234}; // kf 50
+		int viz_pts[] = {246,286,  380,277,  422,344,  481,328}; // kf 25
+		for (int i = 0; i < 8; i += 2) {
+		ImageRef p = asIR(project(geom.imageToGrid * makeVector(viz_pts[i], viz_pts[i+1], 1.0)));
+		viz_mask[p.y][p.x] = 1;
+		}*/
+
+		//FileCanvas l_samples("out/samples_l.png", l_image.rgb);
+		//FileCanvas r_samples("out/samples_r.png", r_image.rgb);
+
+		/*MatF floor_contrib(geom.grid_size[1], geom.grid_size[0], 0);
+			MatF wall_contrib(geom.grid_size[1], geom.grid_size[0], 0);
+			MatF ceil_contrib(geom.grid_size[1], geom.grid_size[0], 0);*/
+
+		// Compute the NCC payoffs
+		payoffs[0].Resize(geom.grid_size[1], geom.grid_size[0]);
+		payoffs[1].Resize(geom.grid_size[1], geom.grid_size[0]);
+		TIMED("Compute payoffs") INDENTED
+			for (int y = 0; y < geom.grid_size[1] /*l_image.ny()*/; y++) {
+				float* payoffs0_row = payoffs[0][y];
+				float* payoffs1_row = payoffs[1][y];
+
+				// Compute the vertical transfer function for this image row
+				const Vec4& surf_plane = (y < geom.horizon_row /*l_vrect_horizon_y*/) ? ceil_plane : floor_plane;
+				Vec3 pt = makeVector(0,y,1.0);  // this can be any point along the current image row
+				Vec3 surf_pt = IntersectRay(geom.gridToImage/*l_vrect_inv*/*pt, l_cam, surf_plane);
+				const SO3<>& l_rot = l_pc.pose().get_rotation();
+				Vec3 line_nrm = l_rot.inverse() * l_intr.T() * geom.imageToGrid/*l_vrect*/.T() * makeVector(0,-1.0,y);
+				Vec3 plane_nrm = unit(makeVector(line_nrm[0], line_nrm[1], 0));
+				Vec4 plane_eqn = concat(plane_nrm, -plane_nrm*surf_pt);
+				Mat3 ltr_wall = GetHomographyVia(l_image.pc(), r_image.pc(), plane_eqn);
+
+				// These transform from grid coordinates to l_vrect and r_vrect
+				Mat3 grid_to_l = l_vrect * geom.gridToImage;
+				Mat3 grid_to_r = r_vrect * ltr_wall * geom.gridToImage;
+
+				// Compute vrect coords
+				int grid_y0, grid_y1;
+				if (y < geom.horizon_row/*l_vrect_horizon_y*/) {
+					grid_y0 = y;
+					grid_y1 = Clamp<int>((y-grid_fToC_ty)/grid_fToC_sy, 0, geom.grid_size[1]-1);
+				} else {
+					grid_y0 = Clamp<int>(grid_fToC_sy*y + grid_fToC_ty, 0, geom.grid_size[1]-1);
+					grid_y1 = y;
+				}
+				CHECK_LE(y0, y1);
+
+				// Compute NCCs for this row
+				for (int x = 0; x < geom.grid_size[0]; x++) {
+					// Compute the remaining transform after x is given
+					//bool special = viz_mask[y][x];
+
+					curry_x[0][1] = x;
+
+					toon::Matrix<3,2> grid_to_ly = grid_to_l * curry_x;
+					grid_to_ly /= grid_to_ly[2][1];
+					CHECK_LE(abs(grid_to_ly[0][0]), 1e-8);  // require output x independent of input y
+					CHECK_LE(abs(grid_to_ly[2][0]), 1e-8);  // require output x independent of input y
+					int lx = roundi(grid_to_ly[0][1]);
+					float l_m = grid_to_ly[1][0];
+					float l_c = grid_to_ly[1][1];
+
+					toon::Matrix<3,2> grid_to_ry = grid_to_r * curry_x;
+					grid_to_ry /= grid_to_ry[2][1];
+					CHECK_LE(abs(grid_to_ry[0][0]), 1e-8);  // require output x independent of input y
+					CHECK_LE(abs(grid_to_ry[2][0]), 1e-8);  // require output x independent of input y
+					int rx = roundi(grid_to_ry[0][1]);
+					float r_m = grid_to_ry[1][0];
+					float r_c = grid_to_ry[1][1];
+
+					// Compute contributions from the horizontal component
+					int vrect_x = lx;
+					int vrect_y0 = Clamp<int>(l_m*grid_y0 + l_c, 0, l_image.ny()-1);
+					int vrect_y1 = Clamp<int>(l_m*grid_y1 + l_c, 0, l_image.ny()-1);
+					NCCStatistics stats;
+					ceil_ncc.AddStats(vrect_x, 0, vrect_y0-1, stats);  // portion above the ceiling point
+					floor_ncc.AddStats(vrect_x, vrect_y1, l_image.ny()-1, stats);  // portion below the floor point
+
+					// debugging...
+					/*Vec2 true_l = project(l_vrect * geom.gridToImage * makeVector(x,y,1.0));
+						Vec2 true_r = project(r_vrect * ltr_wall * geom.gridToImage * makeVector(x,y,1.0));
+						CHECK_EQ(lx, roundi(true_l[0]));
+						CHECK_EQ(rx, roundi(true_r[0]));*/
+
+					// visualization...
+					/*if (special) {
+						Vec3 p0 = makeVector(vrect_x, vrect_y0, 1.0);
+						Vec3 p1 = makeVector(vrect_x, vrect_y1, 1.0);
+						l_samples.DrawDot(project(l_vrect_inv*p0), 3.0, Colors::blue());
+						l_samples.DrawDot(project(l_vrect_inv*p1), 3.0, Colors::blue());
+						r_samples.DrawDot(project(ltr_ceil*l_vrect_inv*p0), 3.0, Colors::blue());
+						r_samples.DrawDot(project(ltr_floor*l_vrect_inv*p1), 3.0, Colors::blue());
+						}*/
+
+					// temp...
+					//NCCStatistics wall_stats;
+
+					// Pull out the rows
+					// RY can be outside image bounds because the images may not entirely overlap
+					if (lx >= 0 && lx < l_vrect_im_tr.Rows() && rx >= 0 && rx < r_vrect_im_tr.Rows()) {
+						const float* l_row = l_vrect_im_tr[lx];  // in transposed image, rows are columns...
+						const float* r_row = r_vrect_im_tr[rx];  // in transposed image, rows are columns...  
+						int l_len = l_vrect_im_tr.Cols();
+						int r_len = r_vrect_im_tr.Cols();
+
+						// Compute contributions from the vertical component
+						for (int yy = grid_y0; yy <= grid_y1; yy++) {  // '<=' justified because y1 always < geom.grid_size[1]
+							int ly = l_m*yy + l_c;
+							int ry = r_m*yy + r_c;
+
+							if (ly >= 0 && ly < l_len && l_row[ly] >= 0 &&
+									ry >= 0 && ry < r_len && r_row[ry] >= 0) {
+								// l_m measures the number of pixels in the vrect domain
+								// that each grid pixel corresponds to. Therefore we weight
+								// each grid measurement by l_m.
+								stats.Add(l_row[ly], r_row[ry], l_m);  // accessing row from transposed image where y=column
+								//wall_stats.Add(l_row[ly], r_row[ry], l_m);
+
+								/*if (special) {
+									Vec3 p = makeVector(x, yy, 1.0);
+									l_samples.DrawDot(project(l_vrect_inv * makeVector(lx,ly,1.0)), 2.0, Colors::red());
+									r_samples.DrawDot(project(r_vrect_inv * makeVector(rx,ry,1.0)), 2.0, Colors::red());
+									}*/
+							}
+						}
+					}
+
+					/*ceil_contrib[y][x] = 1.0 + ceil_ncc.CalculateNCC(vrect_x, 0, vrect_y0-1);
+						floor_contrib[y][x] = 1.0 + floor_ncc.CalculateNCC(vrect_x, vrect_y1, l_image.ny()-1);
+						wall_contrib[y][x] = 1.0 + wall_stats.CalculateNCC();*/
+
+					if (stats.sum_wts == 0) {
+						payoffs0_row[x] = payoffs1_row[x] = 0;
+					} else {
+						// NOTE: I think it makes sense to take the absolute NCC since
+						// a large negative NCC suggests an anti-correlation, which
+						// indicates a good match. But I'm not sure...
+						// I'm now adding 1, for convenience and since the DP objective function is additive
+						payoffs0_row[x] = payoffs1_row[x] = 1.0 + stats.CalculateNCC();
+						CHECK_PRED1(isfinite, payoffs0_row[x]) << "[x="<<x<<",y="<<y<<"], stats:"<<stats;
+					}
+				}
+			}
+
+		/*WriteMatrixImageRescaled("out/contrib_ceil.png", ceil_contrib);
+			WriteMatrixImageRescaled("out/contrib_floor.png", floor_contrib);
+			WriteMatrixImageRescaled("out/contrib_wall.png", wall_contrib);*/
+	}
+
+
+	void StereoPayoffs::OutputPayoffs(const string& file) {
+		FileCanvas payoffs_canvas(file, l_input->rgb);
+		for (int y = 0; y < l_input->ny(); y += 5) {
+			for (int x = 0; x < l_input->nx(); x += 5) {
+				Vec2 vrect_p = project(geometry->imageToGrid * makeVector(x,y,1.0));
+				double payoff = payoffs[0][ roundi(vrect_p[1]) ][ roundi(vrect_p[0]) ];
+				if (payoff >= 0) {
+					PixelRGB<byte> color(0,payoff*255,0);  // payoffs are in [0,1]
+					payoffs_canvas.DrawDot(makeVector(x,y), color);
+				}
+			}
+		}
+	}
+
+	void StereoPayoffs::OutputRawPayoffs(const string& file) {
+		WriteMatrixImageRescaled(file, payoffs[0]);
+	}
+
+}  // namespace indoor_context
